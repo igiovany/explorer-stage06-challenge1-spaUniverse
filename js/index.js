f573afa2c963d875182c284264b4b72fcf3a850f
Giovany Lawall Inocêncio <ino.giovany@gmail.com>
2023-10-17 22:28:53 -0300

    Challenge Completed

--- js/index.js
+++ js/index.js
@@ -2,11 +2,10 @@ import { Router } from './router.js'
 
 const router = new Router()
 router.add('/', "/pages/home.html")
-router.add('/about', "/pages/about.html")
-router.add('/contact', "/pages/contact.html")
-router.add(404, "/pages/404.html")
+router.add('/universe', "/pages/universe.html")
+router.add('/exploration', "/pages/exploration.html")
 
-console.log(router)
+// console.log(router)
 
 router.handle()
 
--- js/router.js
+++ js/router.js
@@ -1,27 +1,50 @@
+const body = document.querySelector('body')
+
 export class Router {
   routes = {}
 
   add(routeName, page) {
-    	this.routes[routeName] = page
+    this.routes[routeName] = page
   }
 
   route(event) {
     event = event || window.event
     event.preventDefault()
-  
+
     window.history.pushState({}, "", event.target.href)
-  
+
     this.handle()
   }
 
   handle() {
     const { pathname } = window.location
-    const route = this.routes[pathname] || this.routes[404]
-  
+    const route = this.routes[pathname]
+
+    this.toggleBackground(pathname)
+
     fetch(route).then((data) => data.text())
-    .then(html => {
-      document.querySelector('#app').innerHTML = html
-    })
-  
+      .then(html => {
+        document.querySelector('#app').innerHTML = html
+      })
+  }
+
+  toggleBackground(pathname) {
+    const oldClass = body.classList.value
+    body.classList.remove(`${oldClass}`)
+
+    switch (pathname) {
+      case "/":
+        body.classList.add('home')
+        break;
+      case "/universe":
+        body.classList.add("universe");
+        break;
+      case "/exploration":
+        body.classList.add("exploration");
+        break;
+
+      default:
+        break;
+    }
   }
 }
